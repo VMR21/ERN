54ea0aa224afc1bb3788c925a5aefaa3adcc9664
VMR21 <reddymodith@gmail.com>
2025-05-13 05:02:11 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3fffc0",
+        dark: "#0b0b0b",
+        accent: "#ff5ad4",
+      },
+      fontFamily: {
+        heading: ["Orbitron", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
