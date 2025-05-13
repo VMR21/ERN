1239f3a75e028d465acff7210bd8dede72d77b15
VMR21 <reddymodith@gmail.com>
2025-05-14 01:16:35 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home.jsx";
 import Leaderboard from "./pages/Leaderboard.jsx";
+import Header from "./components/Header.jsx"; // ✅ import
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
+      <Header /> {/* ✅ Show header on all pages */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/leaderboard" element={<Leaderboard />} />
