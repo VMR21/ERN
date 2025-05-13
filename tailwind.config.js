08addaf655ce400409feda9b2701c773c9c390b8
VMR21 <reddymodith@gmail.com>
2025-05-13 06:15:52 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {
       colors: {
         primary: "#3fffc0",
         dark: "#0b0b0b",
-        accent: "#ff5ad4",
+        accent: "#ff5ad4"
       },
       fontFamily: {
-        heading: ["Orbitron", "sans-serif"],
-      },
-    },
+        heading: ["Orbitron", "sans-serif"]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
